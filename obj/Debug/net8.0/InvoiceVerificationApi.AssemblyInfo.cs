5de1ae494b4890a992da092ee5564a782ef7ab0b
Fatma Yılmaz <79848362+fatmayilmaz4101@users.noreply.github.com>
2024-08-09 17:09:45 +0300

    created project

--- obj/Debug/net8.0/InvoiceVerificationApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/InvoiceVerificationApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceVerificationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7f54a10206f2eaf8f1d84f5502ccdb8872ab22")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceVerificationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceVerificationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
